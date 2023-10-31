91f8c714e1aa29809f66a6dbc18822b0fd5f3349
kimutaidarius <kimutaidarius7@gmail.com>
2023-10-30 20:49:36 -0700

    cp

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * error_file - checks if files can be opened.
+ * @file_from: file_from.
+ * @file_to: file_to.
+ * @argv: arguments vector.
+ * Return: no return.
+ */
+void error_file(int file_from, int file_to, char *argv[])
+{
+	if (file_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+}
+
+/**
+ * main - Copies the content of a file to another file.
+ * @argc: Argument count.
+ * @argv: Argument vector.
+ *
+ * Description: functions as described above.
+ * Return: 1 on success, -1 on failure.
+ */
+int main(int argc, char *argv[])
+{
+	int file_from, file_to, err_close;
+	ssize_t nchars, nwr;
+	char buf[1024];
